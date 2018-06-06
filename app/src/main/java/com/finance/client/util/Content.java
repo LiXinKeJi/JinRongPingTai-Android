7cddde59d475806124e52c82fccd603376c91394
Slingge <linke13523089752>
2018-06-06 11:50:33 +0800

    修改服务器ip

--- app/src/main/java/com/finance/client/util/Content.java
+++ app/src/main/java/com/finance/client/util/Content.java
@@ -6,7 +6,7 @@ package com.finance.client.util;
  */
 
 public class Content {
-    public static final String DOMAIN = "http://122.114.49.11:8091/bankService/service.action";
+    public static final String DOMAIN = "http://39.108.181.225/bankService/service.action";
 //    public static final String DOMAIN = "http://192.168.3.190:8080/bankService/service.action";
 public static String WXID = "wxf33600c85feaf21b";
 }
